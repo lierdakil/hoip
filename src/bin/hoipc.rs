88318624efbf99aeaa2488b84dbe303dd9e9cc4d
Nikolay Yakimov <root@livid.pp.ru>
2025-11-29 06:49:28 +0300

    Docs

--- src/bin/hoipc.rs
+++ src/bin/hoipc.rs
@@ -8,18 +8,25 @@ use tokio_util::codec::Framed;
 
 #[derive(clap::Parser)]
 struct Cli {
+    /// Address/port to listen on. `0.0.0.0` is any address.
     #[arg(long, short)]
     listen: String,
+    /// Name of the virtual device.
     #[arg(long, short, default_value = "hoipc")]
     name: String,
+    /// Bus type of the virtual device.
     #[arg(long, short, default_value = "BUS_USB")]
     bus: BusType,
+    /// Vendor ID of the virtual device.
     #[arg(long, short, default_value_t = 1)]
     vendor_id: u16,
+    /// Product ID of the virtual device.
     #[arg(long, short, default_value_t = 1)]
     product_id: u16,
+    /// Product version of the virtual device.
     #[arg(long, default_value_t = 1)]
     product_version: u16,
+    /// Disable high-resolution scrolling events in the device description.
     #[arg(long)]
     no_high_res_scroll: bool,
 }
