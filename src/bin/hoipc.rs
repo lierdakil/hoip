f58eaf2ddb0620f579dfe43079604e7eb0cabb8b
Nikolay Yakimov <root@livid.pp.ru>
2025-11-29 06:46:40 +0300

    Refactor

--- src/bin/hoipc.rs
+++ src/bin/hoipc.rs
@@ -30,31 +30,31 @@ async fn main() -> anyhow::Result<()> {
 
     let config = Cli::parse();
 
-    let connect = async || {
-        let (tcp_stream, remote) = {
-            let listener = tokio::net::TcpListener::bind(&config.listen).await?;
-            tracing::info!(address = config.listen, "Started listener");
-            listener.accept().await?
-        };
-        tracing::info!(%remote, "Accepted remote connection");
-        let mut framed = Framed::new(tcp_stream, Codec);
-        let dev = builder(&config)?;
-        let dev = dev.build()?;
-        tracing::info!("Created virtual device");
-        let mut udev_stream = dev.into_event_stream()?;
-        tracing::info!("Starting event loop");
-        while let Some(next) = framed.try_next().await? {
-            udev_stream.device_mut().emit(&[next])?;
-        }
-        tracing::info!(%remote, "Connection closed normally");
-        anyhow::Ok(())
-    };
-
     loop {
-        if let Err(e) = connect().await {
+        if let Err(e) = connect(&config).await {
             tracing::error!(%e);
         }
     }
+}
+
+async fn connect(config: &Cli) -> anyhow::Result<()> {
+    let (tcp_stream, remote) = {
+        let listener = tokio::net::TcpListener::bind(&config.listen).await?;
+        tracing::info!(address = config.listen, "Started listener");
+        listener.accept().await?
+    };
+    tracing::info!(%remote, "Accepted remote connection");
+    let mut framed = Framed::new(tcp_stream, Codec);
+    let dev = builder(config)?;
+    let dev = dev.build()?;
+    tracing::info!("Created virtual device");
+    let mut udev_stream = dev.into_event_stream()?;
+    tracing::info!("Starting event loop");
+    while let Some(next) = framed.try_next().await? {
+        udev_stream.device_mut().emit(&[next])?;
+    }
+    tracing::info!(%remote, "Connection closed normally");
+    anyhow::Ok(())
 }
 
 fn builder(config: &Cli) -> anyhow::Result<evdev::uinput::VirtualDeviceBuilder<'_>> {
