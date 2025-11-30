8072fd7d88d19a6f6779386057402b023e220cf0
Nikolay Yakimov <root@livid.pp.ru>
2025-11-30 10:26:28 +0300

    Simplify codec

--- src/codec.rs
+++ src/codec.rs
@@ -1,5 +1,4 @@
-use evdev::{EventType, InputEvent};
-use serde::{Deserialize, Serialize};
+use evdev::InputEvent;
 use tokio_util::{
     bytes::{Buf, BufMut},
     codec::{Decoder, Encoder},
@@ -15,39 +14,13 @@ impl Encoder<InputEvent> for Codec {
         item: InputEvent,
         dst: &mut tokio_util::bytes::BytesMut,
     ) -> Result<(), Self::Error> {
-        postcard::to_io(&InputEventWrap(item), dst.writer())?;
+        let t: u16 = item.event_type().0;
+        let c: u16 = item.code();
+        let v: i32 = item.value();
+        dst.put_u16(t);
+        dst.put_u16(c);
+        dst.put_i32(v);
         Ok(())
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-#[repr(transparent)]
-struct InputEventWrap(#[serde(with = "InputEventDef")] InputEvent);
-
-#[derive(Serialize, Deserialize)]
-#[serde(remote = "InputEvent")]
-struct InputEventDef {
-    #[serde(getter = "InputEvent::event_type")]
-    type_: EventType,
-    #[serde(getter = "InputEvent::code")]
-    code: u16,
-    #[serde(getter = "InputEvent::value")]
-    value: i32,
-}
-
-impl From<InputEvent> for InputEventDef {
-    fn from(value: InputEvent) -> Self {
-        InputEventDef {
-            type_: value.event_type(),
-            code: value.code(),
-            value: value.value(),
-        }
-    }
-}
-
-impl From<InputEventDef> for InputEvent {
-    fn from(value: InputEventDef) -> Self {
-        InputEvent::new_now(value.type_.0, value.code, value.value)
     }
 }
 
@@ -60,15 +33,13 @@ impl Decoder for Codec {
         &mut self,
         src: &mut tokio_util::bytes::BytesMut,
     ) -> Result<Option<Self::Item>, Self::Error> {
-        match postcard::take_from_bytes(&*src) {
-            Ok((InputEventWrap(res), rest)) => {
-                src.advance(src.len() - rest.len());
-                Ok(Some(res))
-            }
-            Err(err) => match err {
-                postcard::Error::DeserializeUnexpectedEnd => Ok(None),
-                e => Err(e.into()),
-            },
+        if src.remaining() < 8 {
+            Ok(None)
+        } else {
+            let t = src.get_u16();
+            let c = src.get_u16();
+            let v = src.get_i32();
+            Ok(Some(InputEvent::new_now(t, c, v)))
         }
     }
 }
